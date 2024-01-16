3db7c8502caef7f36c4d98cc0a88ce524e5901e8
leowndev <leonardogt2308@gmail.com>
2024-01-16 19:42:10 -0300

    Move fadein animation to a new file

---
+++ app/animations/fadeIn.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function FadeIn({ children }: { children: React.ReactNode }) {
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ ease: "easeInOut", duration: 0.74 }}
+    >
+      {children}
+    </motion.div>
+  );
+}
--- app/template.tsx
+++ app/template.tsx
@@ -1,15 +1,5 @@
-"use client";
-
-import { motion } from "framer-motion";
+import FadeIn from "./animations/fadeIn";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return (
-    <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ ease: "easeInOut", duration: 0.74 }}
-    >
-      {children}
-    </motion.div>
-  );
+  return <FadeIn>{children}</FadeIn>;
 }
