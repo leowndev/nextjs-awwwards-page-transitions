2b9502b30918ea0bc8341c05ef8e64ac678eea93
leowndev <leonardogt2308@gmail.com>
2024-01-21 12:58:09 -0300

    Animate page transitions

--- app/template.tsx
+++ app/template.tsx
@@ -1,5 +1,19 @@
-import FadeIn from "./animations/fadeIn";
+"use client";
+import { useEffect } from "react";
+import { animatePageIn } from "@/animations";
 
 export default function Template({ children }: { children: React.ReactNode }) {
-  return <FadeIn>{children}</FadeIn>;
+  useEffect(() => {
+    animatePageIn();
+  }, []);
+
+  return (
+    <div>
+      <div
+        id="transition-element"
+        className="w-screen h-screen bg-red-500 z-100 fixed left-0 top-0"
+      />
+      {children}
+    </div>
+  );
 }
